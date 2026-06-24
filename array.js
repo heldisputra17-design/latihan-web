2abecc4438d4e47519105a23f3a3b661fb755dd1
latihan web <heldisputra17@gmail.com>
2026-06-24 14:41:46 +0800

    array

---
+++ array.js
@@ -0,0 +1,47 @@
+var angka = [2,5,1,3,4]
+console.log(angka[1])
+console.log(angka[0])
+console.log(angka)
+
+//length
+console.log(angka.length)
+
+//push
+console.log("Sebelum push :"+ angka)
+angka.push(9)
+console.log("setelah push :"+ angka)
+
+//pop
+console.log("Sebelum pop :"+ angka)
+angka.pop()
+console.log("Setelah pop :"+ angka)
+
+//unshift
+console.log("Sebelum unshift :"+ angka)
+angka.unshift(8)
+console.log("Setelah unshift :"+ angka)
+
+//shift
+console.log("Sebelum shift :"+ angka)
+angka.shift()
+console.log("Setelah shift :"+ angka)
+
+//join
+var peserta = ["Andra","Taufik","Adit","Debi"]
+console.log(peserta)
+
+console.log("Sebelum join")
+console.log(peserta)
+
+console.log("Setelah Join")
+console.log(peserta.join("-"))
+
+//split
+var nama = "Muhammad Desta Greddy Aulia Rahman"
+console.log("Sebelum split")
+console.log(nama)
+console.log("Sesudah Split")
+console.log(nama.split(" "))
+
+var nama = "Muhammad Desta Greddy Aulia Rahman"
+console.log(angka.length)
