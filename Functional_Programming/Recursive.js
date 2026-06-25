80e061502b1ccdbad2fcf50a5938bcbfec32a6a1
latihan web <heldisputra17@gmail.com>
2026-06-25 13:55:16 +0800

    save functional

--- Functional_Programming/Recursive.js
+++ Functional_Programming/Recursive.js
@@ -1,3 +1,4 @@
+//Recursive
 function countDown(number) {
     console.log(number);
     var newNumber = number - 1;
@@ -7,3 +8,48 @@ function countDown(number) {
 }
 
 countDown(10);
+
+//First-class Function
+ function hitungpersegi(sisi){
+     function luas(s){
+        return s * s;
+    }
+    function keliling(s){
+        return 4 * s;
+    }
+  
+    console.log("luas persegi = ",luas(sisi))
+    console.log("keliling persegi = ",keliling(sisi))
+ }
+ hitungpersegi(10);
+
+// function hitungPersegi(sisi) {
+//     function luas(s) {
+//         return s * s;
+//     }
+//     function keliling(s) {
+//         return 4 * s;
+//     }
+  
+//     console.log("Luas persegi = ", luas(sisi));
+//     console.log("Keliling persegi = ", keliling(sisi));
+// }
+
+// hitungPersegi(6);
+
+//CURRYING FUNCTION
+// function tambah (a, b){
+//     return a + b
+// }
+
+// console.log(tambah(2,5))
+
+// function tambah (a){
+//     return function (b){
+//         return a+b
+//     }
+// }
+
+// console.log(tambah(2)(5))
+
+//
