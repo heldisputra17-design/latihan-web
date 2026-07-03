ce9bf174966c2fab612eba5e593a01e18c9ea903
latihan web <heldisputra17@gmail.com>
2026-07-03 15:36:36 +0800

    save router movie

---
+++ my-express-app/src/routes/movieRouter.js
@@ -0,0 +1,19 @@
+const express = require ("express")
+const movieRouter = express.Router()
+
+const {
+   getMovieApi,
+   getMovieById,
+   getMovieByIdApi,
+   getMovies,
+   timeMiddleware,
+   yearMiddleware,
+   tokenMiddleware,
+   loggerMiddleware
+} = require ('../controllers/movieController')
+
+movieRouter.get('/movies', getMovieApi)
+movieRouter.get('/movies:id', getMovieByIdApi)
+
+module.exports = { movieRouter }
+
