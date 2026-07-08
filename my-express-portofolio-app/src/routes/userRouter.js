70ab9d9fee73da3f0729985863677e3846690462
latihan web <heldisputra17@gmail.com>
2026-07-08 15:16:40 +0800

    Materi CSS 8/7/26

--- my-express-portofolio-app/src/routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const userRouter = express.Router()
-const {login, register} = require('../controllers/userController.js')
-
-userRouter.get('/login', login)
-userRouter.get('/register', register)
-
-module.exports = userRouter
