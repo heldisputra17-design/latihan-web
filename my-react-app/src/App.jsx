046bdde7311c8e85d7c8150da81ec0b4903f4366
latihan web <heldisputra17@gmail.com>
2026-07-10 14:08:37 +0800

    materi 10/7/26

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+import Welcome from './components/Welcome'
+
+function App() {
+  let peserta = [
+    {nama:"Rendra", kota:"Samarinda", umur:15},
+    {nama:"Andra", kota:"Balikpapan", umur:16},
+    {nama:"Desta", kota:"Samarinda", umur:17},
+    {nama:"Daffa", kota:"Samarinda", umur:18},
+    {nama:"Irti", kota:"Makassar", umur:19},
+    {nama:"Yul", kota:"Malinau", umur:20}
+  ]
+
+
+  return  (
+    <>
+      <div className='container'>
+        {peserta.map((item,index) => {
+          return(
+          <Welcome key={index} name={item.nama} city={item.kota} age={item.umur}></Welcome>    
+          )
+          })
+        }
+
+      </div>
+    </>
+  )
+}
+
+export default App
